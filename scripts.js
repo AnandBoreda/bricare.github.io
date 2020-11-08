863c5380990b5fdac870ddb756d922c6da1f065d
anandboreda <zz.anand2016@gmail.com>
2020-11-08 20:15:28 +0530

    pendulum animation

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var cough = document.getElementById('cough');
+var smile = document.getElementById('smile');
+
+function swinging(){
+    var cough = document.getElementById('cough');
+    // var smile = document.getElementById('smile');
+    cough.style.transform = " rotate(32deg)";
+}
+
